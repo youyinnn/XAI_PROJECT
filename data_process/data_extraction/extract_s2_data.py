c7f6983141f147660b5665b88febd136ec013d7d
youyinnn <youyinnn@gmail.com>
2022-04-10 10:18:11 -0400

    update

--- data_process/data_extraction/extract_s2_data.py
+++ data_process/data_extraction/extract_s2_data.py
@@ -1,13 +1,22 @@
-from data_process.data_completion.db_s2 import get_unchecked_partition, insert_data, check_partition,get_data
+from data_process.data_completion.db_s2 import get_unchecked_partition, insert_data, check_partition, get_data
 from data_process.data_completion.db_s2 import get_data_count, get_data_with_ids
 import urllib.request
-import shutil, os, gzip,json,zlib, time, random, multiprocessing,re
+import shutil
+import os
+import gzip
+import json
+import zlib
+import time
+import random
+import multiprocessing
+import re
 from multiprocessing import Pool
 from functools import reduce
 from data_process.conf import cates
 
 from data_process.data_completion.db_s2 import get_all_titles_by_partitions, get_checked_partition
 data_loading_line_limit = 100000
+
 
 def extract():
     unchecked_partition = get_unchecked_partition()
@@ -20,7 +29,8 @@ def extract():
         count += 1
         gz_name = f'{partition}.gz'
         url = f"https://s3-us-west-2.amazonaws.com/ai2-s2-research-public/open-corpus/2022-01-01/{gz_name}"
-        file_store_path = os.path.join(os.environ.get("DATA_DIR"), f"{gz_name}")
+        file_store_path = os.path.join(
+            os.environ.get("DATA_DIR"), f"{gz_name}")
         file_store_path_str = str(file_store_path)
 
         print(f'downloading.. {gz_name} to {file_store_path_str}')
@@ -45,7 +55,7 @@ def extract():
                     authors_list = []
                     for author in jso['authors']:
                         authors_list.append(author['name'])
-                    
+
                     jso['abstract'] = jso['paperAbstract']
                     jso['s2_id'] = jso['id']
                     jso['authors'] = json.dumps(authors_list)
@@ -69,9 +79,12 @@ def extract():
                     # del jso['fieldsOfStudy']
 
                     if jso['venue'] != '' and jso['abstract'] != '' and jso['year'] != None:
-                        jso['title'] = zlib.compress(str.encode(jso['title'], encoding='utf-8'), 9)
-                        jso['abstract'] = zlib.compress(str.encode(jso['abstract'], encoding='utf-8'), 9)
-                        jso['authors'] = zlib.compress(str.encode(jso['authors'], encoding='utf-8'), 9)
+                        jso['title'] = zlib.compress(
+                            str.encode(jso['title'], encoding='utf-8'), 9)
+                        jso['abstract'] = zlib.compress(
+                            str.encode(jso['abstract'], encoding='utf-8'), 9)
+                        jso['authors'] = zlib.compress(
+                            str.encode(jso['authors'], encoding='utf-8'), 9)
                         cs_data.append(jso)
 
         # print(len(cs_data))
@@ -86,20 +99,24 @@ def extract():
         print('\n')
         # break
 
+
 def export(start, amount):
     amount_left = int(amount)
     start_for_this_round = int(start)
-    output_s2_incomplete_data_file_name = os.path.join(os.environ.get("DATA_DIR"), 's2_sample', f"completed_s2_{int(start) + 1}_to_{int(start) + int(amount)}#incomplete.data")
+    output_s2_incomplete_data_file_name = os.path.join(os.environ.get(
+        "DATA_DIR"), 's2_sample', f"completed_s2_{int(start) + 1}_to_{int(start) + int(amount)}#incomplete.data")
     if os.path.exists(output_s2_incomplete_data_file_name):
         os.remove(output_s2_incomplete_data_file_name)
 
-    output_s2_complete_data_file_name = os.path.join(os.environ.get("DATA_DIR"), 's2_sample', f"completed_s2_{int(start) + 1}_to_{int(start) + int(amount)}.data")
+    output_s2_complete_data_file_name = os.path.join(os.environ.get(
+        "DATA_DIR"), 's2_sample', f"completed_s2_{int(start) + 1}_to_{int(start) + int(amount)}.data")
     while amount_left > 0:
         data_out = []
         amount_for_this_round = data_loading_line_limit if amount_left > data_loading_line_limit else amount_left
         data = get_data(start_for_this_round, amount_for_this_round)
 
         amount_left -= data_loading_line_limit
+        amount_left = amount_left if amount_left > 0 else 0
         print(f'{amount_left} left')
         start_for_this_round += amount_for_this_round
         for d in data:
@@ -109,10 +126,11 @@ def export(start, amount):
         with open(output_s2_incomplete_data_file_name, "a+") as f:
             f.write("\n".join(data_out) + '\n')
         data = []
-        
-    
-    os.rename(output_s2_incomplete_data_file_name, output_s2_complete_data_file_name)
-    print(output_s2_complete_data_file_name + f" saved with {amount} completed data")
+
+    os.rename(output_s2_incomplete_data_file_name,
+              output_s2_complete_data_file_name)
+    print(output_s2_complete_data_file_name +
+          f" saved with {amount} completed data")
 
 
 # this will take 600M more memories
@@ -135,10 +153,13 @@ def export_rand():
         data_out.append(json.dumps(d))
 
     rand_sequence = hex(zlib.crc32(str(time.time()).encode('utf-8')))
-    output_s2_data_file_name = os.path.join(os.environ.get("DATA_DIR"), 's2_sample', f"completed_s2_rand.{rand_sequence}.data")
+    output_s2_data_file_name = os.path.join(os.environ.get(
+        "DATA_DIR"), 's2_sample', f"completed_s2_rand.{rand_sequence}.data")
     with open(output_s2_data_file_name, "w") as leaned_raw_topic_data:
         leaned_raw_topic_data.write("\n".join(data_out))
-        print(output_s2_data_file_name + f" saved with {len(data_out)} completed data")
+        print(output_s2_data_file_name +
+              f" saved with {len(data_out)} completed data")
+
 
 def export_with_id(ids_file):
     ids = []
@@ -147,15 +168,18 @@ def export_with_id(ids_file):
             ids.append(id)
 
     head, tail = os.path.split(ids_file)
-    output_s2_incomplete_data_file_name = os.path.join(os.environ.get("DATA_DIR"), 's2_sample', f"completed_s2_with_id.{tail}#incomplete.data")
+    output_s2_incomplete_data_file_name = os.path.join(os.environ.get(
+        "DATA_DIR"), 's2_sample', f"completed_s2_with_id.{tail}#incomplete.data")
     if os.path.exists(output_s2_incomplete_data_file_name):
         os.remove(output_s2_incomplete_data_file_name)
-    output_s2_complete_data_file_name = os.path.join(os.environ.get("DATA_DIR"), 's2_sample', f"completed_s2_with_id.{tail}.data")
+    output_s2_complete_data_file_name = os.path.join(os.environ.get(
+        "DATA_DIR"), 's2_sample', f"completed_s2_with_id.{tail}.data")
 
     start_idx = 0
     ids_left = len(ids)
     while ids_left > 0:
-        end_idx = start_idx + data_loading_line_limit if ids_left > data_loading_line_limit else start_idx + ids_left
+        end_idx = start_idx + \
+            data_loading_line_limit if ids_left > data_loading_line_limit else start_idx + ids_left
         data = get_data_with_ids(ids[start_idx: end_idx])
         ids_left -= data_loading_line_limit
         print(f'{ids_left} left')
@@ -168,9 +192,12 @@ def export_with_id(ids_file):
 
         with open(output_s2_incomplete_data_file_name, "a+") as f:
             f.write("\n".join(data_out) + '\n')
-    
-    os.rename(output_s2_incomplete_data_file_name, output_s2_complete_data_file_name)
-    print(output_s2_complete_data_file_name + f" saved with {len(ids)} completed data")
+
+    os.rename(output_s2_incomplete_data_file_name,
+              output_s2_complete_data_file_name)
+    print(output_s2_complete_data_file_name +
+          f" saved with {len(ids)} completed data")
+
 
 def g_title(title):
     new_s = []
@@ -179,6 +206,7 @@ def g_title(title):
             new_s.append(c)
     new_s = ''.join(new_s).lower()
     return new_s
+
 
 def search_matching_result_with_title_in_s2_by_partition(partition_name_list_and_matched_jso_data):
     partition_name_list = partition_name_list_and_matched_jso_data[0]
@@ -197,10 +225,12 @@ def search_matching_result_with_title_in_s2_by_partition(partition_name_list_and
                 },
                 's2': data
             })
-    
-    print(f'finished searching from: {partition_name_list[0]} to {partition_name_list[len(partition_name_list) - 1]} in {len(titles_by_partition)} s2 data')
+
+    print(
+        f'finished searching from: {partition_name_list[0]} to {partition_name_list[len(partition_name_list) - 1]} in {len(titles_by_partition)} s2 data')
 
     return final_matched_result
+
 
 def matching_arxiv_data_by_topic(cate, src):
     print("extracting data from " + src + "\nby cate regx: " + cate['regex'])
@@ -236,9 +266,9 @@ def matching_arxiv_data_by_topic(cate, src):
         process_map_arg.append(
             [
                 partition[
-                    current_partition_start_index: 
+                    current_partition_start_index:
                     current_partition_start_index + duty_number_for_worker
-                ], 
+                ],
                 matched_jso_data
             ]
         )
@@ -246,7 +276,8 @@ def matching_arxiv_data_by_topic(cate, src):
         i += 1
 
     with Pool(p_number) as pool:
-        rs = pool.map_async(search_matching_result_with_title_in_s2_by_partition, process_map_arg)
+        rs = pool.map_async(
+            search_matching_result_with_title_in_s2_by_partition, process_map_arg)
         data = reduce(lambda a, b: a+b, rs.get())
 
     data_out = []
@@ -255,19 +286,22 @@ def matching_arxiv_data_by_topic(cate, src):
     for d in data:
         data_out.append(str(d['s2']['id']))
         data_v_out.append(str(d['s2']['id']) + ' ' + d['arxiv']['categories'])
-        data_v_out.append('a:' + d['arxiv']['title'].replace('\n', '').replace('  ', ' '))
+        data_v_out.append(
+            'a:' + d['arxiv']['title'].replace('\n', '').replace('  ', ' '))
         data_v_out.append('s:' + d['s2']['title'].replace('\n', ''))
         data_v_out.append('\n')
 
     percentage = round(len(data_out)/len(matched_jso_data) * 100, 2)
     print(f'({len(data_out)}/{len(matched_jso_data)}) ({percentage}%) records are also found in s2 dataset')
 
-    output_data_file_name = os.path.join(os.environ.get("DATA_DIR"), 'index', "arxiv_" + cate['name'] + "_data_in_s2id.txt")
+    output_data_file_name = os.path.join(os.environ.get(
+        "DATA_DIR"), 'index', "arxiv_" + cate['name'] + "_data_in_s2id.txt")
     with open(output_data_file_name, "w") as leaned_raw_data:
         leaned_raw_data.write("\n".join(data_out))
         print(output_data_file_name + " saved")
 
-    output_data_file_name = os.path.join(os.environ.get("DATA_DIR"), 'index', "arxiv_" + cate['name'] + "_data_in_s2id_title_compare.txt")
+    output_data_file_name = os.path.join(os.environ.get(
+        "DATA_DIR"), 'index', "arxiv_" + cate['name'] + "_data_in_s2id_title_compare.txt")
     with open(output_data_file_name, "w") as leaned_raw_data:
         leaned_raw_data.write("\n".join(data_v_out))
         print(output_data_file_name + " saved")
@@ -275,8 +309,10 @@ def matching_arxiv_data_by_topic(cate, src):
     spend_t = round(time.time() - st, 4)
     print(f'cost {spend_t} sec')
 
+
 def extract_from_arxiv_cate_id_list(cate_name, amount=None):
-    src = os.path.join(os.environ.get("DATA_DIR"), 'index',  "arxiv_" + cate_name + "_data_in_s2id.txt")
+    src = os.path.join(os.environ.get("DATA_DIR"), 'index',
+                       "arxiv_" + cate_name + "_data_in_s2id.txt")
     id_list = []
     with open(src) as f:
         lines = f.readlines()
@@ -299,11 +335,13 @@ def extract_from_arxiv_cate_id_list(cate_name, amount=None):
         d['authors'] = json.loads(d['authors'])
         data_out.append(json.dumps(d))
 
-    
-    output_s2_data_file_name = os.path.join(os.environ.get("DATA_DIR"), 's2_sample', f"completed_s2_arxiv_{cate_name}_{sufix}_.data")
+    output_s2_data_file_name = os.path.join(os.environ.get(
+        "DATA_DIR"), 's2_sample', f"completed_s2_arxiv_{cate_name}_{sufix}_.data")
     with open(output_s2_data_file_name, "w") as leaned_raw_topic_data:
         leaned_raw_topic_data.write("\n".join(data_out))
-        print(output_s2_data_file_name + f" saved with {len(data_out)} completed data")
+        print(output_s2_data_file_name +
+              f" saved with {len(data_out)} completed data")
+
 
 def count_arxiv_data_by_cate():
     count = []
@@ -312,7 +350,8 @@ def count_arxiv_data_by_cate():
     for c in cates:
         if c != 'cs':
             cate_count += 1
-            src = os.path.join(os.environ.get("DATA_DIR"), 'index',  "arxiv_" + c + "_data_in_s2id.txt")
+            src = os.path.join(os.environ.get("DATA_DIR"),
+                               'index',  "arxiv_" + c + "_data_in_s2id.txt")
             with open(src) as f:
                 lines = f.readlines()
                 data_count += len(lines)
